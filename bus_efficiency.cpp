df016ec95a8f6d9aabeab9baed5f44b98a535768
KIMdolphin <rlacofls0212@naver.com>
2019-11-06 23:06:45 +0900

    main 화면 구성

--- bus_efficiency.cpp
+++ bus_efficiency.cpp
@@ -1,22 +1,116 @@
 #include <stdio.h>
+#include <cstdlib>
+#include <windows.h>
+#include <conio.h>
 
-int main() {
-	printf("■■■■■■■    ■■       ■■ ■■■■■■■\n");
-	printf("■■        ■■ ■■        ■■ ■■■■■■■■\n");
-	printf("■■        ■■ ■■        ■■ ■■           \n");
-	printf("■■■■■■■   ■■        ■■ ■■■■■■■■\n");
-	printf("■■          ■ ■■■      ■■ ■■■■■■■■\n");
-	printf("■■          ■ ■■■      ■■            ■■\n");
-	printf("■■        ■■ ■■■      ■■ ■■■■■■■\n");
-	printf("■■■■■■■     ■■■■■■■ ■■■■■■ \n\n");
+#define UP 1
+#define DOWN 2
+#define ENTER 3
+
+
+void ui();
+void title();
+void gotoxy(int, int);
+int menu();
+int keyControl();
+
+int main(){
+	ui();
+	title();
+	menu();
+}
+
+void ui() {
+	system("mode con cols=200 lines=120 | title bus efficiency");
+}
+
+void title() {
+	printf("\n");
+	printf(" ■■■■■■■    ■■       ■■ ■■■■■■■\n");
+	printf(" ■■        ■■ ■■        ■■ ■■■■■■■■\n");
+	printf(" ■■        ■■ ■■        ■■ ■■           \n");
+	printf(" ■■■■■■■   ■■        ■■ ■■■■■■■■\n");
+	printf(" ■■          ■ ■■■      ■■ ■■■■■■■■\n");
+	printf(" ■■          ■ ■■■      ■■            ■■\n");
+	printf(" ■■        ■■ ■■■      ■■ ■■■■■■■\n");
+	printf(" ■■■■■■■     ■■■■■■■ ■■■■■■ \n\n");
 	
-	printf("■■■■■■■ ■■■■■■■ ■■■■■■■ ■■■■■■■ ■■■■■■■ ■■■■■■■ ■■■■■■■ ■■■■      ■ ■■■■■■■ ■■     ■■\n");
-	printf("■■           ■■           ■■                ■■      ■■                ■■      ■■           ■■■■      ■ ■■           ■■    ■■\n");
-	printf("■■           ■■■■■■■ ■■■■■■■      ■■      ■■                ■■      ■■           ■■  ■■    ■ ■■             ■■ ■■\n");
-	printf("■■■■■■■ ■■■■■■   ■■■■■■        ■■      ■■                ■■      ■■■■■■■ ■■   ■■   ■ ■■             ■■ ■■\n");
-	printf("■■■■■■■ ■■           ■■                ■■      ■■                ■■      ■■■■■■■ ■■   ■■   ■ ■■               ■■\n");
-	printf("■■           ■■           ■■                ■■      ■■                ■■      ■■           ■■    ■■  ■ ■■               ■■\n");
-	printf("■■           ■■           ■■           ■■■■■■■ ■■■■■■■ ■■■■■■■ ■■           ■■     ■■ ■ ■■■■■■■     ■■\n");
-	printf("■■■■■■■ ■■           ■■           ■■■■■■■ ■■■■■■■ ■■■■■■■ ■■■■■■■ ■■      ■■■ ■■■■■■■     ■■ \n\n");
-	printf("made by 2-1-8 chaerin KIM");
+	printf(" ■■■■■■■ ■■■■■■■ ■■■■■■■ ■■■■■■■ ■■■■■■■ ■■■■■■■ ■■■■■■■ ■■■■      ■ ■■■■■■■ ■■     ■■\n");
+	printf(" ■■           ■■           ■■                ■■      ■■                ■■      ■■           ■■■■      ■ ■■           ■■    ■■\n");
+	printf(" ■■           ■■■■■■■ ■■■■■■■      ■■      ■■                ■■      ■■           ■■  ■■    ■ ■■             ■■ ■■\n");
+	printf(" ■■■■■■■ ■■■■■■   ■■■■■■        ■■      ■■                ■■      ■■■■■■■ ■■   ■■   ■ ■■             ■■ ■■\n");
+	printf(" ■■■■■■■ ■■           ■■                ■■      ■■                ■■      ■■■■■■■ ■■   ■■   ■ ■■               ■■\n");
+	printf(" ■■           ■■           ■■                ■■      ■■                ■■      ■■           ■■    ■■  ■ ■■               ■■\n");
+	printf(" ■■           ■■           ■■           ■■■■■■■ ■■■■■■■ ■■■■■■■ ■■           ■■     ■■ ■ ■■■■■■■     ■■\n");
+	printf(" ■■■■■■■ ■■           ■■           ■■■■■■■ ■■■■■■■ ■■■■■■■ ■■■■■■■ ■■      ■■■ ■■■■■■■     ■■ \n\n");
+	printf("         developed by 2-1-8 chaerin KIM");
+}
+
+void gotoxy(int x, int y){
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD pos;
+	pos.X=x;
+	pos.Y=y;
+	SetConsoleCursorPosition(consoleHandle, pos);
+}
+
+int menu(){
+	int x=100;
+	int y=20;
+	gotoxy(x-5,y);
+	printf("> |");
+	gotoxy(x,y);
+	printf("프로그램 설명");
+	gotoxy(x+15,y);
+	printf("|");
+	gotoxy(x-3,y+2);
+	printf("|");
+	gotoxy(x,y+2);
+	printf("프로그램 시작");
+	gotoxy(x+15,y+2);
+	printf("|");
+	gotoxy(x-3,y+4);
+	printf("|");
+	gotoxy(x+4,y+4);
+	printf("종료");
+	gotoxy(x+15,y+4);
+	printf("|");
+	while (1) { 
+		int ch = keyControl();
+        switch (ch) {           
+            case DOWN: {
+        		if(y<24){
+        			gotoxy(x-5, y);
+        			printf(" ");
+        			gotoxy(x-5, y+2);
+					printf(">"); 
+				}
+				break;
+			}
+            case UP: {
+                if(y>20){
+                	gotoxy(x-5, y);
+        			printf(" ");
+        			gotoxy(x-5, y-2);
+					printf(">"); 
+				}
+				break;
+            }
+        }
+    }
+}
+
+
+
+int keyControl() {
+	char temp = _getch();
+	if (temp == 's' || temp == 'S'){
+		return DOWN;
+	}
+	else if ( temp == 'w' || temp == 'W'){
+		return UP;
+	}
+	else if (temp == '\n'){
+		return ENTER;
+	}
 }
